d867b2c13b11470bdfabd3eef123151b4258ff12
Dominik Chrastecky <dominik@chrastecky.cz>
2022-02-08 18:39:47 +0100

    Better progress, language filters

--- src/Command/DownloadCommand.php
+++ src/Command/DownloadCommand.php
@@ -2,12 +2,15 @@
 
 namespace App\Command;
 
+use App\DTO\DownloadDescription;
 use App\DTO\GameDetail;
+use App\Enum\Language;
 use App\Enum\OperatingSystem;
 use App\Service\DownloadManager;
 use App\Service\OwnedItemsManager;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -52,6 +55,18 @@ final class DownloadCommand extends Command
                     )
                 )
             )
+            ->addOption(
+                'language',
+                'l',
+                InputOption::VALUE_REQUIRED,
+                'Download only games for specified language. See command "languages" for list of them.',
+            )
+            ->addOption(
+                'language-fallback-english',
+                null,
+                InputOption::VALUE_NONE,
+                'Download english versions of games when the specified language is not found.',
+            )
         ;
     }
 
@@ -61,32 +76,78 @@ final class DownloadCommand extends Command
 
         $noVerify = $input->getOption('no-verify');
         $operatingSystem = OperatingSystem::tryFrom($input->getOption('os'));
+        $language = Language::tryFrom($input->getOption('language'));
+        $englishFallback = $input->getOption('language-fallback-english');
+
+        if ($language !== null && $language !== Language::English && !$englishFallback) {
+            $io->warning("GOG often has multiple language versions inside the English one. Those game files will be skipped. Specify --language-fallback-english to include English versions if your language's version doesn't exist.");
+        }
 
         foreach ($this->ownedItemsManager->getLocalGameData() as $game) {
-            foreach ($game->downloads as $download) {
-                $progress = $io->createProgressBar();
-                $progress->setFormat(
-                    ' %current% MB / %max% MB [%bar%] %percent:3s%% - %message%'
+            $downloads = $game->downloads;
+
+            if ($englishFallback && $language) {
+                $downloads = array_filter(
+                    $game->downloads,
+                    fn (DownloadDescription $download) => $download->language === $language->getLocalName()
                 );
+                if (!count($downloads)) {
+                    $downloads = array_filter(
+                        $game->downloads,
+                        fn (DownloadDescription $download) => $download->language === Language::English->getLocalName(),
+                    );
+                }
+            }
+
+            foreach ($downloads as $download) {
+                $progress = $io->createProgressBar();
+                ProgressBar::setPlaceholderFormatterDefinition(
+                    'bytes_current',
+                    $this->getBytesCallable($progress->getProgress(...)),
+                );
+                ProgressBar::setPlaceholderFormatterDefinition(
+                    'bytes_total',
+                    $this->getBytesCallable($progress->getMaxSteps(...)),
+                );
+
+                $format = ' %bytes_current% / %bytes_total% [%bar%] %percent:3s%% - %message%';
+                $progress->setFormat($format);
 
                 $targetFile = "{$this->getTargetDir($input, $game)}/{$this->downloadManager->getFilename($download)}";
                 $startAt = null;
                 if (($download->md5 || $noVerify) && file_exists($targetFile)) {
                     $md5 = $noVerify ? '' : md5_file($targetFile);
                     if (!$noVerify && $download->md5 === $md5) {
-                        $io->writeln(
-                            "{$download->name} ({$download->platform}, {$download->language}): Skipping because it exists and is valid",
-                        );
+                        if ($output->isVerbose()) {
+                            $io->writeln(
+                                "{$download->name} ({$download->platform}, {$download->language}): Skipping because it exists and is valid",
+                            );
+                        }
                         continue;
                     } elseif ($noVerify) {
-                        $io->writeln("{$download->name} ({$download->platform}, {$download->language}): Skipping because it exists (--no-verify specified, not checking content)");
+                        if ($output->isVerbose()) {
+                            $io->writeln("{$download->name} ({$download->platform}, {$download->language}): Skipping because it exists (--no-verify specified, not checking content)");
+                        }
                         continue;
                     }
                     $startAt = filesize($targetFile);
                 }
 
                 if ($operatingSystem !== null && $download->platform !== $operatingSystem->value) {
-                    $io->writeln("{$download->name} ({$download->platform}, {$download->language}): Skipping because of OS filter");
+                    if ($output->isVerbose()) {
+                        $io->writeln("{$download->name} ({$download->platform}, {$download->language}): Skipping because of OS filter");
+                    }
+                    continue;
+                }
+
+                if (
+                    $language !== null
+                    && $download->language !== $language->getLocalName()
+                    && (!$englishFallback || $download->language !== Language::English->getLocalName())
+                ) {
+                    if ($output->isVerbose()) {
+                        $io->writeln("{$download->name} ({$download->platform}, {$download->language}): Skipping because of language filter");
+                    }
                     continue;
                 }
 
@@ -94,11 +155,11 @@ final class DownloadCommand extends Command
                 $progress->setProgress(0);
                 $progress->setMessage("{$download->name} ({$download->platform}, {$download->language})");
 
-                $responses = $this->downloadManager->download($download, function (int $current, int $total) use ($progress) {
+                $responses = $this->downloadManager->download($download, function (int $current, int $total) use ($progress, $output) {
                     if ($total > 0) {
-                        $progress->setMaxSteps($total / 2**20);
+                        $progress->setMaxSteps($total);
+                        $progress->setProgress($current);
                     }
-                    $progress->setProgress($current / 2**20);
                 }, $startAt);
 
                 if (file_exists($targetFile)) {
@@ -142,5 +203,34 @@ final class DownloadCommand extends Command
         }
 
         return $dir;
+    }
+
+    private function getBytesCallable(callable $targetMethod): callable
+    {
+        return function (ProgressBar $progressBar, OutputInterface $output) use ($targetMethod) {
+            $coefficient = 1;
+            $unit = 'B';
+
+            $value = $targetMethod();
+            if (!$output->isVeryVerbose()) {
+                if ($value > 2**10) {
+                    $coefficient = 2**10;
+                    $unit = 'kB';
+                }
+                if ($value > 2**20) {
+                    $coefficient = 2**20;
+                    $unit = 'MB';
+                }
+                if ($value > 2**30) {
+                    $coefficient = 2**30;
+                    $unit = 'GB';
+                }
+            }
+
+            $value = $value / $coefficient;
+            $value = number_format($value, 2);
+
+            return "{$value} {$unit}";
+        };
     }
 }
