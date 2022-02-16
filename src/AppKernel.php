866d02031d2d77056d4a76899e1420a7bc27ad84
Dominik Chrastecky <dominik@chrastecky.cz>
2022-02-16 12:51:31 +0100

    Use http 1

--- src/AppKernel.php
+++ src/AppKernel.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use App\DependencyInjection\CommandLocatorCompilerPass;
+use Closure;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\HttpKernel\Kernel;
 
 final class AppKernel extends Kernel
@@ -17,7 +20,24 @@ final class AppKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load(__DIR__ . '/../config/services.yaml');
+        $loader->load(function (ContainerBuilder $container) use ($loader) {
+            $kernelLoader = $loader->getResolver()->resolve(__FILE__);
+            assert($kernelLoader instanceof PhpFileLoader);
+            $kernelLoader->setCurrentDir(__DIR__);
+            /** @noinspection PhpPassByRefInspection */
+            $instanceof = &Closure::bind(function &() {
+                return $this->instanceof;
+            }, $kernelLoader, $kernelLoader)();
+
+            $this->configureContainer(new ContainerConfigurator(
+                $container,
+                $kernelLoader,
+                $instanceof,
+                __FILE__,
+                __FILE__,
+                $this->getEnvironment(),
+            ));
+        });
     }
 
     public function getCacheDir(): string
@@ -28,6 +48,19 @@ final class AppKernel extends Kernel
     public function getLogDir(): string
     {
         return sys_get_temp_dir() . "/{$this->getAppKey()}/logs";
+    }
+
+    protected function configureContainer(ContainerConfigurator $container): void
+    {
+        $container->import('../config/{packages}/*.yaml');
+        $container->import('../config/{packages}/' . $this->environment . '/*.yaml');
+
+        if (is_file(\dirname(__DIR__) . '/config/services.yaml')) {
+            $container->import('../config/services.yaml');
+            $container->import('../config/{services}_' . $this->environment . '.yaml');
+        } elseif (is_file($path = \dirname(__DIR__) . '/config/services.php')) {
+            (require $path)($container->withPath($path), $this);
+        }
     }
 
     protected function build(ContainerBuilder $container)
