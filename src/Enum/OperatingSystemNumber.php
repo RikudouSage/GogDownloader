c7060fd99a6c689eee29235594aea43eb947e9b9
Rikudou_Sage <dominik@chrastecky.cz>
2025-02-01 13:15:54 +0100

    Add new operating systems (#33)

--- src/Enum/OperatingSystemNumber.php
+++ src/Enum/OperatingSystemNumber.php
@@ -8,11 +8,13 @@ enum OperatingSystemNumber: int
     case WindowsVista = 2 ** 1;
     case Windows7 = 2 ** 2;
     case Windows8 = 2 ** 3;
+    case Windows10 = 2 ** 12;
+    case Windows11 = 2 ** 14;
     case OsX10_6 = 2 ** 4;
     case OsX10_7 = 2 ** 5;
     case Ubuntu14 = 2 ** 10;
     case Ubuntu16 = 2 ** 11;
-    case Windows10 = 2 ** 12;
     case Ubuntu18 = 2 ** 13;
-    case Windows11 = 2 ** 14;
+    case Ubuntu20 = 2 ** 15;
+    case Ubuntu22 = 2 ** 16;
 }
