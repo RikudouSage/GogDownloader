9e8012f968c1638bf613925569dd5e0a578e5b03
Dominik Chrastecky <dominik@chrastecky.cz>
2025-03-02 13:18:34 +0100

    Chore: Saner setting of a hash

--- src/DTO/DownloadDescription.php
+++ src/DTO/DownloadDescription.php
@@ -4,26 +4,21 @@ namespace App\DTO;
 
 final class DownloadDescription
 {
-    public readonly string $md5;
-
     public function __construct(
         public readonly string $language,
         public readonly string $platform,
         public readonly string $name,
         public readonly float  $size,
         public readonly string $url,
-        ?string                $md5,
-        public ?int            $gogGameId,
+        public private(set) ?string $md5,
+        public readonly ?int   $gogGameId,
     ) {
-        if ($md5 !== null) {
-            $this->md5 = $md5;
-        }
     }
 
     public function __unserialize(array $data): void
     {
         if (is_string($data['gogGameId'])) {
-            $data['gogGameId'] = (int) $data['gogGameId'];
+            $data['gogGameId'] = (int)$data['gogGameId'];
         }
 
         foreach ($data as $key => $value) {
